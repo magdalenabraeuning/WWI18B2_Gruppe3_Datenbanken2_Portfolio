a9262d0b7d149a808f4f26fcf608ad1cbc58d1dd
Magdalena Bräuning <braeuning.magdalena@dh-karlsruhe.de>
2020-10-12 17:26:41 +0200

    Commit 2 - ConnectionHelper

---
+++ src/main/java/connection/ConnectionHelper.java
@@ -0,0 +1,28 @@
+package connection;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+
+
+
+public class ConnectionHelper {
+
+    private static final Logger LOGGER = LogManager.getLogger(ConnectionHelper.class);
+
+
+    public static EntityManager getConnection() {
+        EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory( "ORMExamplePU" );
+        EntityManager em = entityManagerFactory.createEntityManager();
+
+        LOGGER.info( "# --- Connection established ---" );
+
+        return em;
+    }
+
+
+}
